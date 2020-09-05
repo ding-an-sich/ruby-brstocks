4634e1b1b9d0fcaa732b754adc55df211090a888
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-09-05 15:51:01 -0300

    Prints consolidated positions

--- mech-cei.rb
+++ mech-cei.rb
@@ -6,8 +6,9 @@ require 'io/console'
 puts 'username'
 username = gets.chomp
 pass = STDIN.getpass("pass\n")
+puts 'Logging in...'
 
-# Initialize Mechanize agent for crawling
+# Initialize Mechanize agent for crawling and populate SSL certs
 agent = Mechanize.new
 agent.user_agent_alias = 'Mac Safari'
 cert_store = OpenSSL::X509::Store.new
@@ -15,12 +16,22 @@ cert_store.add_file 'certs/cei-b3-com-br.pem'
 cert_store.add_file 'certs/cei-b3-com-br-chain.pem'
 agent.cert_store = cert_store
 
-# Crawl CEI
+# Submits login form
 cei = agent.get 'https://cei.b3.com.br/CEI_Responsivo/'
 login_form = cei.form('aspnetForm')
 login_form.field_with(id: 'ctl00_ContentPlaceHolder1_txtLogin').value = username
 login_form.field_with(id: 'ctl00_ContentPlaceHolder1_txtSenha').value = pass
 Timeout.timeout(10) do
+  # Using a timeout to deal with CEI unresponsiveness
   cei = agent.submit(login_form, login_form.buttons.first)
 end
-pp cei
+
+# Get and print consolidated position
+positions = cei.xpath('//tbody/tr/td')
+positions.pop # We don't want CEI advertising
+puts "\n\n"
+positions.each do |position|
+  puts position.text
+end
+
+puts "\nFinished!"
